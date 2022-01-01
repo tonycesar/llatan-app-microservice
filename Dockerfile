ab191839ce06c63e2756e6e990147ab0f154bfa0
César Calle <mcalle@ulasalle.edu.pe>
2022-01-01 10:12:11 -0500

    fix: add https with letsencrypt

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,9 @@ RUN npm install
 COPY . .
 
 ENV PORT 8080
+ENV PORT_SSL 8083
 
 EXPOSE 8080
+EXPOSE 8083
 
 CMD [ "node", "app.js" ]
--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const fs = require('fs');
+const https = require('https');
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
+
 module.exports = app; // for testing
 
 var config = {
@@ -15,9 +18,21 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   swaggerExpress.register(app);
 
   var port = process.env.PORT || 10010;
+  var portSSL = process.env.PORT_SSL;
   app.listen(port);
 
   if (swaggerExpress.runner.swagger.paths['/hello']) {
     console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
   }
+  if (portSSL) {
+    const privateKey = fs.readFileSync('/mysite/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/mysite/cert.pem', 'utf8');
+    const ca = fs.readFileSync('/mysite/chain.pem', 'utf8');
+    const credentials = { key: privateKey, cert: certificate, ca: ca };
+    const httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(portSSl, () => { console.log('HTTPS Server running on port 443'); });
+  }
+
+
+
 });
